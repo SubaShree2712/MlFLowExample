81239659024c2f9edfd448cb5c67af296652dcab
SubaShree2712 <74586256+SubaShree2712@users.noreply.github.com>
2021-03-29 13:28:09 +0530

    Update MlFlow.py

--- MlFlow.py
+++ MlFlow.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     print("Multiplication :"+str(c))
     #path='C:\Users\smurugan\Desktop\Image_classification\mlflow\reviewed_415_dents_csv.csv'
     output=sys.argv[3]
+    with open(output,'rb')
     mlflow.log_artifact(output)
     cwd = os.getcwd()    
     print(cwd)
